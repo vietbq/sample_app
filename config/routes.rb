a1fd39e3d03cf9f47bc061173b26c7e93557a5de
buiquocviet <buiquocviet1993@gmail.com>
2015-12-16 16:38:14 +0700

    Login, log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
@@ -10,6 +12,10 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
