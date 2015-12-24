dc843e4ef8d6663b2b21580f135dea23bf5e7c2b
buiquocviet <buiquocviet1993@gmail.com>
2015-12-24 16:53:15 +0700

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
  	attr_accessor :remember_token, :activation_token, :reset_token
   	before_save   :downcase_email
   	before_create :create_activation_digest
@@ -23,17 +31,20 @@ class User < ActiveRecord::Base
   def User.new_token
    SecureRandom.urlsafe_base64
   end
+
 	# Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
+
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
@@ -61,14 +72,31 @@ class User < ActiveRecord::Base
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
 	# Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
   # Defines a proto-feed.
   # See "Following users" for the full implementation.
   def feed
     microposts
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   # Private functions
